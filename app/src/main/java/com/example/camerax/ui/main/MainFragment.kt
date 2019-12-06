df217ab07de298f5c597ada1d8a52b1f21438cae
wzq <wzq_1990@sina.com>
2019-12-06 16:52:54 +0800

    init

--- app/src/main/java/com/example/camerax/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/camerax/ui/main/MainFragment.kt
@@ -1,6 +1,5 @@
 package com.example.camerax.ui.main
 
-import android.graphics.Matrix
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.view.LayoutInflater
@@ -11,7 +10,6 @@ import androidx.camera.core.*
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.camerax.R
-import kotlinx.android.synthetic.main.main_fragment.*
 import java.io.File
 import kotlin.math.abs
 import kotlin.math.max
@@ -25,12 +23,11 @@ class MainFragment : Fragment() {
     }
 
     private val filePath by lazy {
-        File(
-            requireActivity().externalMediaDirs.first(), "test.png"
-//            requireActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),
-//            "test.png"
-        )
+        File(requireActivity().externalMediaDirs.first(), "test.png")
     }
+
+    private val lensFacing = CameraX.LensFacing.BACK
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,91 +41,43 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    private lateinit var imageCapture: ImageCapture
+
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val lensFacing = CameraX.LensFacing.BACK
         val executor = ContextCompat.getMainExecutor(requireContext())
-        
-        camera.addOnLayoutChangeListener { v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom ->
-            updateTransform(camera)
+        val cameraView = view?.findViewById<TextureView>(R.id.camera) ?: return
+        cameraView.setOnClickListener {
+            imageCapture.takePicture(
+                filePath,
+                executor,
+                imageSavedListener
+            )
         }
-        view?.post {
 
-            val metrics = DisplayMetrics().also { camera.display.getRealMetrics(it) }
-            val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
-            println("ratio ---> ${screenAspectRatio.name}")
+        cameraView.post {
+//            val metrics = DisplayMetrics().also { cameraView.display.getRealMetrics(it) }
+            val screenAspectRatio = aspectRatio(cameraView.width, cameraView.height)
 
-            val viewFinderConfig = PreviewConfig.Builder().apply {
+            val previewConfig = PreviewConfig.Builder().apply {
                 setLensFacing(lensFacing)
-                // We request aspect ratio but no resolution to let CameraX optimize our use cases
                 setTargetAspectRatio(screenAspectRatio)
-                // Set initial target rotation, we will have to call this again if rotation changes
-                // during the lifecycle of this use case
-                setTargetRotation(camera.display.rotation)
+                setTargetRotation(cameraView.display.rotation)
             }.build()
-//            val preview = AutoFitPreviewBuilder.build(viewFinderConfig, camera)
+            val preview = AutoFitPreviewBuilder.build(previewConfig, cameraView)
 
-            val preview = Preview(viewFinderConfig).also {
-                it.setOnPreviewOutputUpdateListener { et ->
-                    val parent = camera.parent as? ViewGroup
-                    parent?.removeAllViews()
-                    parent?.addView(camera)
-                    camera.surfaceTexture = et.surfaceTexture
-                    updateTransform(camera)
-                }
-            }
-
-            val imageCapture = ImageCapture(
+            imageCapture = ImageCapture(
                 ImageCaptureConfig.Builder()
-                    .setTargetRotation(camera.display.rotation)
                     .setLensFacing(lensFacing)
                     .setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
                     .setTargetAspectRatio(screenAspectRatio)
+                    .setTargetRotation(cameraView.display.rotation)
                     .build())
-
-//            val analyzerConfig = ImageAnalysisConfig.Builder().apply {
-//                setLensFacing(lensFacing)
-//                // In our analysis, we care more about the latest image than analyzing *every* image
-//                setImageReaderMode(ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
-//                // Set initial target rotation, we will have to call this again if rotation changes
-//                // during the lifecycle of this use case
-//                setTargetRotation(camera.display.rotation)
-//            }.build()
-//
-//            val imageAnalyzer = ImageAnalysis(analyzerConfig).apply {
-//                setAnalyzer(executor,
-//                    LuminosityAnalyzer { luma ->
-//                        // Values returned from our analyzer are passed to the attached listener
-//                        // We log image analysis results here --
-//                        // you should do something useful instead!
-//                        (analyzer as LuminosityAnalyzer).framesPerSecond
-//                    })
-//            }
-
 
             CameraX.bindToLifecycle(viewLifecycleOwner, preview, imageCapture)
 
-            message.setOnClickListener {
-                imageCapture.takePicture(
-                    filePath,
-                    executor,
-                    imageSavedListener
-                )
-            }
         }
-    }
-
-    private fun updateTransform(viewFinder: TextureView) {
-        val matrix = Matrix()
-        // Compute the center of the view finder
-        val centerX: Float = viewFinder.getWidth() / 2f
-        val centerY: Float = viewFinder.getHeight() / 2f
-        val rotations = floatArrayOf(0f, 90f, 180f, 270f)
-        // Correct preview output to account for display rotation
-        val rotationDegrees = rotations[viewFinder.getDisplay().getRotation()]
-        matrix.postRotate(-rotationDegrees, centerX, centerY)
-        // Finally, apply transformations to our TextureView
-        viewFinder.setTransform(matrix)
     }
 
     private val imageSavedListener = object : ImageCapture.OnImageSavedListener {
@@ -149,11 +98,11 @@ class MainFragment : Fragment() {
 
 
     private fun aspectRatio(width: Int, height: Int): AspectRatio {
-        val ratio_4_3 = 4.0 / 3.0
-        val ratio_16_9 = 16.0 / 9.0
+        val ratio43 = 4.0 / 3.0
+        val ratio169 = 16.0 / 9.0
         val previewRatio = max(width, height).toDouble() / min(width, height)
 
-        if (abs(previewRatio - ratio_4_3) <= abs(previewRatio - ratio_16_9)) {
+        if (abs(previewRatio - ratio43) <= abs(previewRatio - ratio169)) {
             return AspectRatio.RATIO_4_3
         }
         return AspectRatio.RATIO_16_9
